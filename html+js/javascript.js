d0f43a2a526e86cb30789b90a390305c95b776df
RONALDO NUNES BELEM <98597641+RonaldoBelem@users.noreply.github.com>
2024-03-08 21:45:36 -0300

    Add files via upload

---
+++ html+js/javascript.js
@@ -0,0 +1,12 @@
+const body = document.querySelector("body");
+
+body.innerHTML = 
+`<h1> texto h1 </h1>
+<ul> texto ul </ul> 
+<a href="www.google.com"> link tag a  </a> 
+<ol> texto ol</ol> `
+
+
+
+
+
